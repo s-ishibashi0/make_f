edfa4926c7567ac7a8263474df8674a60c331b79
k-minoda <fko2447028@stu.o-hara.ac.jp>
2025-06-04 14:45:05 +0900

    test reg

--- WebContent/scoremanager/main/test_regist.jsp
+++ WebContent/scoremanager/main/test_regist.jsp
@@ -1,12 +1,33 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:import url="../../base.jsp" />
 
-</body>
-</html>
+<section class="me-4">
+	<h2 class="h3 mb-3 fw-normal bg-secondary bg-opacity-10 py-2 px-4">成績管理</h2>
+	<form method="get">
+		<div class="row border mx-3 mb-3 py-2 align-items-center rounded"
+			id="filter">
+			<div class="col-4">
+				<label class="form-label" for="student-f1-select">入学年度</label> <select
+					class="form-select" id="student-f1-select" name="f1">
+					<option value="0">--------</option>
+					<c:forEach var="year" items="${ent_year_set}">
+						<option value="${year}" <c:if test="${year == f1}">selected</c:if>>${year}</option>
+					</c:forEach>
+				</select>
+			</div>
+
+			<div class="col-4">
+				<label class="form-label" for="student-f2-select">クラス</label> <select
+					class="form-select" id="student-f2-select" name="f2">
+					<option value="0">--------</option>
+					<c:forEach var="num" items="${class_num_set}">
+						<option value="${num}" <c:if test="${num == f2}">selected</c:if>>${num}</option>
+					</c:forEach>
+				</select>
+			</div>
+		</div>
+	</form>
+</section>
+
--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -1,5 +1,98 @@
 package scoremanager.main;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class TestRegistAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-}
+import bean.School;
+import bean.Student;
+import bean.Teacher;
+import dao.ClassNumDAO;
+import dao.StudentDAO;
+import tool.Action;
+
+public class TestRegistAction implements Action {
+
+		    @Override
+		    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		        HttpSession session = request.getSession();
+		        Teacher teacher = (Teacher) session.getAttribute("teacher");
+
+		        System.out.println("teacher: " + teacher);
+		        System.out.println("teacher.getSchool(): " + teacher.getSchool());
+
+		        String entYearStr = request.getParameter("f1");
+		        String classNum = request.getParameter("f2");
+		        String isAttendStr = request.getParameter("f3");
+
+		        int entYear = 0;
+		        boolean isAttend = false;
+		        List<Student> students = null;
+		        Map<String, String> errors = new HashMap<>();
+
+		        // 現在の年を取得して入学年度リストを作成
+		        LocalDate todayDate = LocalDate.now();
+		        int year = todayDate.getYear();
+		        List<Integer> entYearSet = new ArrayList<>();
+		        for (int i = year - 10; i <= year; i++) {
+		            entYearSet.add(i);
+		        }
+
+		        // DAOの準備
+		        StudentDAO sDao = new StudentDAO();
+		        ClassNumDAO cNumDao = new ClassNumDAO();
+
+		        // Schoolオブジェクトを作成
+		        School school = new School();
+		        school.setCd(teacher.getSchool());
+
+		        System.out.println("学校コード: " + school.getCd());
+
+		        // クラス番号一覧を取得
+		        List<String> classNumList = cNumDao.filter(school);
+
+		        // 入学年度のパース
+		        if (entYearStr != null && !entYearStr.isEmpty()) {
+		            try {
+		                entYear = Integer.parseInt(entYearStr);
+		            } catch (NumberFormatException e) {
+		                errors.put("f1", "入学年度は数値で入力してください");
+		            }
+		        }
+
+		        // 出席情報のパース
+		        if ("t".equals(isAttendStr)) {
+		            isAttend = true;
+		        }
+
+
+		        // 学生リストの取得ロジック
+		        if (entYear != 0 && !classNum.equals("0")) {
+		            students = sDao.filter(school, entYear, classNum, isAttend);
+		        } else if (entYear != 0 && classNum.equals("0")) {
+		            students = sDao.filter(school, entYear, isAttend);
+		        } else if (entYear == 0 && (classNum == null || classNum.equals("0"))) {
+		            students = sDao.filter(school, isAttend);
+		        } else {
+		            errors.put("f1", "クラスを指定する場合は入学年度も指定してください");
+		            students = sDao.filter(school, isAttend);
+		        }
+
+		        // リクエスト属性にセット
+		        request.setAttribute("f1", entYear);
+		        request.setAttribute("f2", classNum);
+		        request.setAttribute("f3", isAttendStr);
+		        request.setAttribute("students", students);
+		        request.setAttribute("class_num_set", classNumList);
+		        request.setAttribute("ent_year_set", entYearSet);
+		        request.setAttribute("errors", errors);
+
+		        return "/scoremanager/main/test_regist.jsp";
+
+		    }
+		}
